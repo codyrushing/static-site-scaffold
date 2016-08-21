2331559d39cdc21796820d00f5ad31875a45b083
Cody Rushing <codyrushing@gmail.com>
2016-08-21 16:39:05 -0500

    fixing sass error handling.  changing normalizeCSS to be more generic, and using better naming conventions. putting "minified vs not" logic in the template

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var path = require("path");
 
 var gulp = require("gulp"),
+    lazypipe = require("lazypipe"),
+    del = require("del"),
     gulpPlugins = require("gulp-load-plugins")(),
     runSequence = require("run-sequence"),
     assembler = require("./assemblefile");
 
 var paths = require("./paths");
 
+const constants = require("./constants");
+const vendorCSSDirectory = path.join(paths.src.styles, "vendor");
 const jsPackageName = "app.js";
+
+var jsProdChannel = lazypipe()
+  .pipe(gulpPlugins.streamify, gulpPlugins.uglify())
+  .pipe(gulpPlugins.rename, {
+    suffix: ".min"
+  })
+  .pipe(gulp.dest, paths.dist.js);
 
 gulp.task("templates", function(done) {
   return assembler.build((err) => {
@@ -18,25 +29,46 @@ gulp.task("templates", function(done) {
 
 // copy normalize.css into our "generated" folder
 // and give it .scss extension so sass can @import it
-gulp.task("normalizeCSS", function(){
-  return gulp.src( path.join(paths.src.lib, "normalize.css/normalize.css") )
-    .pipe(gulpPlugins.rename({
-      extname: ".scss",
-      prefix: "_"
+gulp.task("sassifyVendorCSS", () => {
+  var vendorCSS = [{
+    dependency: "normalize.css",
+    glob: "normalize.css"
+  }];
+
+  let index = 0;
+
+  return gulp.src(
+      vendorCSS.map((item) => path.join(paths.src.lib, item.dependency, item.glob) )
+    )
+    .pipe(gulpPlugins.rename(function(path){
+      path.dirname = vendorCSS[index].dependency;
+      path.extname = ".scss";
+      if(path.basename[0] !== "_"){
+        path.basename = `_${path.basename}`;
+      }
+      index++;
+      return path;
     }))
-    .pipe(gulp.dest( path.join(paths.src.styles, "generated")) );
+    .pipe(gulp.dest(vendorCSSDirectory));
 });
 
 gulp.task("css", (done) => {
-  runSequence("normalizeCSS", "buildCSS", done);
+  runSequence("sassifyVendorCSS", "buildCSS", done);
 });
 
 gulp.task("buildCSS", () => {
   return gulp.src([
-    path.join(paths.src.styles, "*.scss"),
-    "!" + path.join(paths.src.styles, "_*.scss")
-  ])
-    .pipe(gulpPlugins.plumber())
+      path.join(paths.src.styles, "*.scss"),
+      "!" + path.join(paths.src.styles, "_*.scss")
+    ])
+    .pipe(gulpPlugins.plumber({
+      // custom error handler because for some reason in this stream, errors do not end the stream
+      errorHandler: function(err) {
+        gulpPlugins.plumber().errorHandler(err);
+        this.emit("close");
+        this.emit("end");
+      }
+    }))
     .pipe(gulpPlugins.sourcemaps.init())
     .pipe(gulpPlugins.sass({
       includePaths: [paths.lib]
@@ -60,7 +92,8 @@ gulp.task("buildCSS", () => {
     }))
     .pipe(gulpPlugins.sourcemaps.write())
     .pipe(gulpPlugins.cleanCss())
-    .pipe(gulp.dest(paths.dist.css))
+    .pipe(gulpPlugins.notify(`<%= file.relative %> built successfully`))
+    .pipe(gulp.dest(paths.dist.css));
 });
 
 gulp.task("eslint", () => {
@@ -86,11 +119,38 @@ gulp.task("js", ["eslint"], () => {
     .pipe(source(jsPackageName))
     .pipe(gulp.dest(paths.dist.js))
     .pipe(gulpPlugins.notify(`${jsPackageName} built :)`))
-    .pipe(gulpPlugins.streamify(gulpPlugins.uglify()))
-    .pipe(gulpPlugins.rename({
-      suffix: ".min"
-    }))
-    .pipe(gulp.dest(paths.dist.js));
-})
+    .pipe(gulpPlugins.if(!constants.IS_DEVELOPMENT, jsProdChannel()));
+});
 
-gulp.task("dev", ["templates", "css", "js"]);
+gulp.task("watch", (done) => {
+  gulpPlugins.watch(path.join(paths.src.app, "**/*.js"), () => {
+    runSequence("js");
+  });
+  gulpPlugins.watch(path.join(paths.src.styles, "**/*.scss"), () => {
+    runSequence("buildCSS");
+  });
+  gulpPlugins.watch([
+      path.join(paths.src.templates, "**/*.hbs"),
+      path.join(paths.src.content, "**/*.md"),
+      path.join(paths.src.templates, "helpers.js")
+    ],
+    () => {
+      runSequence("templates");
+  });
+  done();
+});
+
+gulp.task("clean", function(){
+  return del([
+    paths.distBase,
+    vendorCSSDirectory
+  ]);
+});
+
+gulp.task("default", ["clean"], function(done){
+  runSequence(["templates", "css", "js"], done);
+});
+
+gulp.task("dev", ["default"], function(done){
+  return runSequence("watch", done);
+});
